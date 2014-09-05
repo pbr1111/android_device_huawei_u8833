c56eb5e8e5ecb050a8ff7c5d797c43494579ed2c
pbr1111 <pbr1111@gmail.com>
2014-09-05 21:23:16 +0200

    Revert "Add TARGET_ARCH_LOWMEM"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,6 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_LOWMEM := true
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
 
